dbba801e02d486527c466485e48912404482fbaf
zl02655931 <327243872@qq.com>
2022-02-12 16:00:47 +0800

    update cross

--- src/main/java/com/itucc/contorller/DefaultContorller.java
+++ src/main/java/com/itucc/contorller/DefaultContorller.java
@@ -1,10 +1,12 @@
 package com.itucc.contorller;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1")
 public class DefaultContorller {
